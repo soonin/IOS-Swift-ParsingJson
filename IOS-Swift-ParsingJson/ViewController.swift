084dd7f0cb158c5053dfa2a9a307da210e850da1
Pooya Hatami <pooya@node-1w7jr9qrok3i9p5vcbqtrebd6.ipv6.telus.net>
2018-03-18 21:46:07 -0700

    Done with course (one Course)

--- IOS-Swift-ParsingJson/ViewController.swift
+++ IOS-Swift-ParsingJson/ViewController.swift
@@ -8,18 +8,59 @@
 
 import UIKit
 
+struct Course: Decodable {
+    let id: Int
+    let name: String
+    let link : String
+    let imageUrl: String
+
+//                // Swift 2/3/Objective C
+//    init(json: [String: Any]) {
+//        id = json["id"] as? Int ??  -1
+//        name = json["name"] as? String ?? ""
+//        link = json["link"] as? String ?? ""
+//        imageUrl = json["imageUrl"] as? String ?? ""
+//    }
+}
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+        let jasonUrlString = "https://raw.githubusercontent.com/soonin/IOS-Swift-ParsingJson/master/IOS-Swift-ParsingJson/course.json"
+        guard  let url = URL(string: jasonUrlString) else {return}
+        
+        URLSession.shared.dataTask(with: url) { (data, respons, err) in
+            //perhaps check err
+            //also perhaps check response
+            
+            guard let data = data else {return}
 
+//            // Swift 4
+//            let dataString = String(data: data , encoding: .utf8)
+//            print(dataString)
+            
+            do {
+                let course = try JSONDecoder().decode(Course.self , from: data)
+                print(course)
+//                // Swift 2/3/Objective C
+//                guard let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String: Any] else {return}
+//
+//                let course = Course(json: json)
+//                print(course.name)
+
+            } catch let jsonErr {
+                print("Error serializing json :",jsonErr)
+            }
+
+            
+        }.resume()
+        
+//        let myCourse =  Course(id: 1, name: "my Course", link: "some Link", imageUrl: "Some image url")
+//        print(myCourse)
+    
+    }
 
 }
 
